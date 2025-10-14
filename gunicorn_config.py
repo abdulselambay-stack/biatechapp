fff17297fd433e7807410ac1518f1dbe9fe7e064
lmelkorl <lmelkorl0@gmail.com>
2025-10-14 20:40:37 +0300

    Add gunicorn config for Railway PORT binding

---
+++ gunicorn_config.py
@@ -0,0 +1,34 @@
+import os
+import multiprocessing
+
+# Server socket
+bind = f"0.0.0.0:{os.environ.get('PORT', '8080')}"
+backlog = 2048
+
+# Worker processes
+workers = 2
+worker_class = 'sync'
+worker_connections = 1000
+timeout = 120
+keepalive = 5
+
+# Logging
+accesslog = '-'
+errorlog = '-'
+loglevel = 'info'
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+
+# Process naming
+proc_name = 'whatsapp-api'
+
+# Server mechanics
+daemon = False
+pidfile = None
+umask = 0
+user = None
+group = None
+tmp_upload_dir = None
+
+# SSL
+keyfile = None
+certfile = None
